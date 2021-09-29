f238f25b3bb659cd324e99695dea19f202c89717
halexuan <hale.pd@gmail.com>
2021-09-29 18:42:08 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-pky1vu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-pky1vu)
---
+++ src/app/shared3.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+console.log('Shared3Service bundled even though not used');
+
+@Injectable()
+export class Shared3Service {
+  constructor() {
+    console.log('SharedService instantiated');
+  }
+}
